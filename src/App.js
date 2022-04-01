186aebd566845c9dbc00daac094c333b239b1f02
yryskul <kuttubaevayryskul@gmail.com>
2022-04-01 14:22:59 +0600

    added content to app.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 
 
 function App() {
+  const example = ["pear", "yellow", "small"];
+  const example2 = ["toyota", "camry", "black", "expensive"]
+
+  const [name, color,size] = example
+  const [mark, modal, color1, price] =example2
+
+
   return (
     <div className="App">
-      
+      This {name}is {color} and {size};
+      this {mark} is {modal} {color1} and {price}
+     
+
     </div>
   );
 }
